034a7395b7befaca88053718162520588c48706c
deulkarraj <141235373+deulkarraj@users.noreply.github.com>
2024-11-19 11:35:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // // index.js
-
 // Step 1: Configure MSAL.js
+
 const msalConfig = {
     auth: {
         clientId: 'ba95a7b7-2c93-43c6-9ba4-7370a6b9a4b6', // Replace with your app's client ID
         authority: 'https://login.microsoftonline.com/12ee7ca7-ad23-44b5-afa6-4f2a2cbdea54', // Replace with your tenant ID or common
         // redirectUri: 'https://deulkarraj.github.io' // Your redirect URI
         // redirectUri: 'http://127.0.0.1:5500/index.html'
-        redirectUri: 'https://businesscentral.dynamics.com/12ee7ca7-ad23-44b5-afa6-4f2a2cbdea54/Development?company=CRONUS%20USA%2C%20Inc.&node=00000000-6136-0000-1516-5400836bd2d2&page=89&dc=0&bookmark=17_pwAAAAJ7_0oAMAAwADAAMQAw'
+        redirectUri: 'https://deulkarraj.github.io/BC-API/'
 
         
     }
